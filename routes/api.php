5c45f6eb265d42b417f5d86cbfce3123537a983d
Mustafiz82 <mustafiz8260@gmail.com>
2024-12-30 13:41:21 +0600

    added authorizaiton for route

--- routes/api.php
+++ routes/api.php
@@ -10,15 +10,19 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::post('/signup' , [UserController::class , "signup"]);
-Route::post('/login' , [UserController::class , "login"]);
-
-Route::post('/study' , [StudyController::class , "createStudyData"]);
-Route::get('/users' , [UserController::class , "getUsersDetails"]);
-Route::get('/user/{id}' , [UserController::class , "getUserDetails"]);
-Route::put('/user/{id}' , [UserController::class , "UpdateUserDetails"]);
-Route::delete('/user/{id}' , [UserController::class , "deleteUserDetails"]);
+Route::post('/signup', [UserController::class, "signup"]);
+Route::post('/login', [UserController::class, "login"]);
 
 
 
 
+Route::group(['middleware' => "auth:sanctum"], function () {
+
+    Route::post('/study', [StudyController::class, "createStudyData"]);
+    Route::get('/users', [UserController::class, "getUsersDetails"]);
+    Route::get('/user/{id}', [UserController::class, "getUserDetails"]);
+    Route::put('/user/{id}', [UserController::class, "UpdateUserDetails"]);
+    Route::delete('/user/{id}', [UserController::class, "deleteUserDetails"]);
+});
+
+// Route::post('/login', [UserController::class, "login"])->name("login");
